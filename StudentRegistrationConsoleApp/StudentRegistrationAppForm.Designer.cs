1a0b23d777dc5948b21b0d50a5c3aa689304540f
ayonelahi <mirelahi33@gmail.com>
2021-11-10 21:48:25 -0800

    Basic form designed

---
+++ StudentRegistrationConsoleApp/StudentRegistrationAppForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace StudentRegistrationConsoleApp
+{
+    partial class StudentRegistrationAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStudents = new System.Windows.Forms.Label();
+            this.labelDepertments = new System.Windows.Forms.Label();
+            this.labelCourses = new System.Windows.Forms.Label();
+            this.labelRegistrations = new System.Windows.Forms.Label();
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            this.dataGridViewCourses = new System.Windows.Forms.DataGridView();
+            this.dataGridViewRegistrations = new System.Windows.Forms.DataGridView();
+            this.dataGridViewDepartments = new System.Windows.Forms.DataGridView();
+            this.buttonAddUpdateStudent = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonDrop = new System.Windows.Forms.Button();
+            this.buttonAddUpdateCourse = new System.Windows.Forms.Button();
+            this.buttonAddUpdateDepartment = new System.Windows.Forms.Button();
+            this.labelInstruction = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCourses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRegistrations)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDepartments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelStudents
+            // 
+            this.labelStudents.AutoSize = true;
+            this.labelStudents.Location = new System.Drawing.Point(39, 23);
+            this.labelStudents.Name = "labelStudents";
+            this.labelStudents.Size = new System.Drawing.Size(49, 13);
+            this.labelStudents.TabIndex = 0;
+            this.labelStudents.Text = "Students";
+            // 
+            // labelDepertments
+            // 
+            this.labelDepertments.AutoSize = true;
+            this.labelDepertments.Location = new System.Drawing.Point(615, 23);
+            this.labelDepertments.Name = "labelDepertments";
+            this.labelDepertments.Size = new System.Drawing.Size(67, 13);
+            this.labelDepertments.TabIndex = 1;
+            this.labelDepertments.Text = "Departments";
+            // 
+            // labelCourses
+            // 
+            this.labelCourses.AutoSize = true;
+            this.labelCourses.Location = new System.Drawing.Point(39, 202);
+            this.labelCourses.Name = "labelCourses";
+            this.labelCourses.Size = new System.Drawing.Size(45, 13);
+            this.labelCourses.TabIndex = 2;
+            this.labelCourses.Text = "Courses";
+            // 
+            // labelRegistrations
+            // 
+            this.labelRegistrations.AutoSize = true;
+            this.labelRegistrations.Location = new System.Drawing.Point(39, 380);
+            this.labelRegistrations.Name = "labelRegistrations";
+            this.labelRegistrations.Size = new System.Drawing.Size(68, 13);
+            this.labelRegistrations.TabIndex = 3;
+            this.labelRegistrations.Text = "Registrations";
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(42, 49);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.Size = new System.Drawing.Size(438, 150);
+            this.dataGridViewStudents.TabIndex = 4;
+            // 
+            // dataGridViewCourses
+            // 
+            this.dataGridViewCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCourses.Location = new System.Drawing.Point(42, 227);
+            this.dataGridViewCourses.Name = "dataGridViewCourses";
+            this.dataGridViewCourses.Size = new System.Drawing.Size(438, 150);
+            this.dataGridViewCourses.TabIndex = 5;
+            // 
+            // dataGridViewRegistrations
+            // 
+            this.dataGridViewRegistrations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRegistrations.Location = new System.Drawing.Point(42, 405);
+            this.dataGridViewRegistrations.Name = "dataGridViewRegistrations";
+            this.dataGridViewRegistrations.Size = new System.Drawing.Size(608, 150);
+            this.dataGridViewRegistrations.TabIndex = 6;
+            // 
+            // dataGridViewDepartments
+            // 
+            this.dataGridViewDepartments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDepartments.Location = new System.Drawing.Point(618, 49);
+            this.dataGridViewDepartments.Name = "dataGridViewDepartments";
+            this.dataGridViewDepartments.Size = new System.Drawing.Size(287, 149);
+            this.dataGridViewDepartments.TabIndex = 7;
+            // 
+            // buttonAddUpdateStudent
+            // 
+            this.buttonAddUpdateStudent.Location = new System.Drawing.Point(509, 106);
+            this.buttonAddUpdateStudent.Name = "buttonAddUpdateStudent";
+            this.buttonAddUpdateStudent.Size = new System.Drawing.Size(85, 62);
+            this.buttonAddUpdateStudent.TabIndex = 8;
+            this.buttonAddUpdateStudent.Text = "Add Or Update Student";
+            this.buttonAddUpdateStudent.UseVisualStyleBackColor = true;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(721, 421);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(90, 36);
+            this.buttonRegister.TabIndex = 10;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // buttonDrop
+            // 
+            this.buttonDrop.Location = new System.Drawing.Point(723, 497);
+            this.buttonDrop.Name = "buttonDrop";
+            this.buttonDrop.Size = new System.Drawing.Size(88, 33);
+            this.buttonDrop.TabIndex = 11;
+            this.buttonDrop.Text = "Drop";
+            this.buttonDrop.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddUpdateCourse
+            // 
+            this.buttonAddUpdateCourse.Location = new System.Drawing.Point(509, 262);
+            this.buttonAddUpdateCourse.Name = "buttonAddUpdateCourse";
+            this.buttonAddUpdateCourse.Size = new System.Drawing.Size(85, 62);
+            this.buttonAddUpdateCourse.TabIndex = 12;
+            this.buttonAddUpdateCourse.Text = "Add Or Update Course";
+            this.buttonAddUpdateCourse.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddUpdateDepartment
+            // 
+            this.buttonAddUpdateDepartment.Location = new System.Drawing.Point(911, 106);
+            this.buttonAddUpdateDepartment.Name = "buttonAddUpdateDepartment";
+            this.buttonAddUpdateDepartment.Size = new System.Drawing.Size(85, 62);
+            this.buttonAddUpdateDepartment.TabIndex = 13;
+            this.buttonAddUpdateDepartment.Text = "Add Or Update Department";
+            this.buttonAddUpdateDepartment.UseVisualStyleBackColor = true;
+            // 
+            // labelInstruction
+            // 
+            this.labelInstruction.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelInstruction.Location = new System.Drawing.Point(878, 421);
+            this.labelInstruction.Name = "labelInstruction";
+            this.labelInstruction.Size = new System.Drawing.Size(96, 109);
+            this.labelInstruction.TabIndex = 14;
+            // 
+            // StudentRegistrationAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 567);
+            this.Controls.Add(this.labelInstruction);
+            this.Controls.Add(this.buttonAddUpdateDepartment);
+            this.Controls.Add(this.buttonAddUpdateCourse);
+            this.Controls.Add(this.buttonDrop);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonAddUpdateStudent);
+            this.Controls.Add(this.dataGridViewDepartments);
+            this.Controls.Add(this.dataGridViewRegistrations);
+            this.Controls.Add(this.dataGridViewCourses);
+            this.Controls.Add(this.dataGridViewStudents);
+            this.Controls.Add(this.labelRegistrations);
+            this.Controls.Add(this.labelCourses);
+            this.Controls.Add(this.labelDepertments);
+            this.Controls.Add(this.labelStudents);
+            this.Name = "StudentRegistrationAppForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.StudentRegistrationAppForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCourses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRegistrations)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDepartments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStudents;
+        private System.Windows.Forms.Label labelDepertments;
+        private System.Windows.Forms.Label labelCourses;
+        private System.Windows.Forms.Label labelRegistrations;
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+        private System.Windows.Forms.DataGridView dataGridViewCourses;
+        private System.Windows.Forms.DataGridView dataGridViewRegistrations;
+        private System.Windows.Forms.DataGridView dataGridViewDepartments;
+        private System.Windows.Forms.Button buttonAddUpdateStudent;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonDrop;
+        private System.Windows.Forms.Button buttonAddUpdateCourse;
+        private System.Windows.Forms.Button buttonAddUpdateDepartment;
+        private System.Windows.Forms.Label labelInstruction;
+    }
+}
+
---
+++ StudentRegistrationConsoleApp/StudentRegistrationAppForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentRegistrationConsoleApp
+{
+    public partial class StudentRegistrationAppForm : Form
+    {
+        public StudentRegistrationAppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentRegistrationAppForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
